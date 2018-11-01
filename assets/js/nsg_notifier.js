3af2e460d4fb4e532ac1945e46e3be51952aa888
Igor Mosunov <imosunov@nsg.ru>
2018-11-01 16:18:35 +0300

    Фиксированный sidebar

--- assets/js/nsg_notifier.js
+++ assets/js/nsg_notifier.js
@@ -1,6 +1,16 @@
 
 $(document).ready(function() {
+
+  $("#sidebar").mCustomScrollbar({
+    theme: "minimal"
+  });
+
   $('#sidebarCollapse').on('click', function () {
-      $('#sidebar').toggleClass('active');
+
+                $('#sidebar, #content').toggleClass('active');
+                $('.collapse.in').toggleClass('in');
+                $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+
+
   });
 });
